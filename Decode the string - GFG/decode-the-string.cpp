6c8595593c60740aa25201fc7843dbce9404a7f3
Preetinder Singh Bajaj <preetindersinghbajaj@gmail.com>
2022-01-22 21:14:52 +0530

    Added solution - LeetHub

---
+++ Decode the string - GFG/decode-the-string.cpp
@@ -0,0 +1,59 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    string  decodestring(string& s,int& i)
+    {
+        string result;
+        while(i<s.length()&& s[i]!=']')
+        {
+            if(isdigit(s[i]))
+            {
+                int k=0;
+                while(i<s.length()&&isdigit(s[i]))
+                    {k= k*10 + s[i++] - '0';}
+                i++;
+                string r=decodestring(s,i);
+                while(k-- > 0)
+                    result+=r;
+                i++;
+            }
+            else
+                {
+                    result+=s[i++];
+                }
+        
+        }
+        return result;
+    }
+    
+    
+    string decodedString(string s){
+        // code here
+        int i=0;
+        return decodestring(s,i);
+        
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        
+        Solution ob;
+        cout<<ob.decodedString(s)<<"\n";
+    }
+    return 0;
+}  // } Driver Code Ends

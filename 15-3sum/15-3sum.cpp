ffcc1bdcfc317e54fb21a96b5cfc73406f35aaad
Preetinder Singh Bajaj <preetindersinghbajaj@gmail.com>
2022-02-03 00:00:37 +0530

    Time: 76 ms (82.31%), Space: 19.9 MB (96.48%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -9,10 +9,10 @@ public:
     sort(nums.begin(),nums.end());
     for(int i=0;i<nums.size()-2;i++)
     {
-        if(i==0 || ((i>0) && nums[i]!=nums[i-1]))
+        if(i==0 || ((i>0) && nums[i]!=nums[i-1]))   // either i first elem or (i>0 and ith not equal to ith -1)
         {
-            l=i+1;
-            r=nums.size()-1;
+            l=i+1;  // l= next ot i 
+            r=nums.size()-1;    // r traverese from right 
             int sum=0-(nums[i]);
             while(l<r)
             {
@@ -23,7 +23,7 @@ public:
                     vec.push_back(nums[r]);
                     ans.push_back(vec);
 
-                    while(l<r && nums[l]==vec[1])
+                    while(l<r && nums[l]==vec[1])   // traverse until duplicates to skip
                         l++;
                     while(l<r && nums[r]==vec[2])
                         r--;

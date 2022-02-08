8355f812c3f444f8b6b517c990dde1362f62c07f
Preetinder Singh Bajaj <preetindersinghbajaj@gmail.com>
2022-02-08 22:55:47 +0530

    Time: 831 ms (41.17%), Space: 167.3 MB (65.72%) - LeetHub

--- 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.cpp
+++ 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.cpp
@@ -1,18 +1,50 @@
+// class Solution {
+// public:
+//     vector<int> findLonely(vector<int>& nums) {
+//         map<int,int>m;
+//         int n=nums.size();
+//         for(int i=0;i<n;i++)
+//         {
+//             m[nums[i]]++;
+//         }
+//         vector<int>ans;
+//         for(int i=0;i<n;i++)
+//         {
+//             if(m[nums[i]]==1 && m[nums[i]-1]==0 && m[nums[i]+1]==0)
+//                 ans.push_back(nums[i]);
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     vector<int> findLonely(vector<int>& nums) {
-        map<int,int>m;
-        int n=nums.size();
-        for(int i=0;i<n;i++)
+        
+        vector<int> ans;
+        unordered_map<int,int> mp;
+        
+        for(int i=0;i<nums.size();i++)
         {
-            m[nums[i]]++;
+            mp[nums[i]]++; //storing in map with frequency
         }
-        vector<int>ans;
-        for(int i=0;i<n;i++)
+        
+        for(auto it:mp)
         {
-            if(m[nums[i]]==1 && m[nums[i]-1]==0 && m[nums[i]+1]==0)
-                ans.push_back(nums[i]);
+            if(it.second==1)  //frequency must be 1
+            {
+                int x=it.first-1;  
+                int y=it.first+1;
+                
+                if(mp.find(x)==mp.end() && mp.find(y)==mp.end())
+                {
+               
+                    ans.push_back(it.first);
+                }
+            }
         }
+        
         return ans;
+        
     }
 };

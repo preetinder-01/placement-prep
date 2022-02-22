9d64bee7be74f91e32f3d3396003505d66931c5d
Preetinder Singh Bajaj <preetindersinghbajaj@gmail.com>
2022-02-22 20:46:00 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (28.55%) - LeetHub

---
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int titleToNumber(string s) {
+        int n = s.length(); // extracting length of the string
+        
+        if(n == 1) // if length is only 1
+        {
+            return (s[0] - 'A') + 1; //then return only that position
+        }
+        
+        int increase = 0; // variable used to increase the multiplcation
+        int ans = 0; // variable used to store answer
+        
+        for(int i = n - 1; i >= 0; i--) // start traversing string from back
+        {
+            int pos = (s[i] - 'A') + 1; // extracting position
+            
+            ans = ans + (pos * pow(26, increase)); // with base 26, do further calculation
+            increase++; // increase variable
+        }
+        
+        return ans; // finally return the answer
+    }
+};

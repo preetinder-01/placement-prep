31c49ddd322a43166fc167d5f95eb2df8e6754a0
Preetinder Singh Bajaj <preetindersinghbajaj@gmail.com>
2022-03-07 01:45:46 +0530

    Added solution - LeetHub

---
+++ Implement Stack using Linked List - GFG/implement-stack-using-linked-list.cpp
@@ -0,0 +1,72 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct StackNode {
+    int data;
+    StackNode *next;
+    StackNode(int a) {
+        data = a;
+        next = NULL;
+    }
+};
+
+class MyStack {
+  private:
+    StackNode *top;
+
+  public:
+    void push(int);
+    int pop();
+    MyStack() { top = NULL; }
+};
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        MyStack *sq = new MyStack();
+
+        int Q;
+        cin >> Q;
+        while (Q--) {
+            int QueryType = 0;
+            cin >> QueryType;
+            if (QueryType == 1) {
+                int a;
+                cin >> a;
+                sq->push(a);
+            } else if (QueryType == 2) {
+                cout << sq->pop() << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+// } Driver Code Ends
+
+
+//Function to push an integer into the stack.
+void MyStack ::push(int x) 
+{
+    // Your Code
+    StackNode *ptr=new StackNode(x);
+    ptr->next=top;
+    top=ptr;
+}
+
+
+//Function to remove an item from top of the stack.
+int MyStack ::pop() 
+{
+    // Your Code
+    if(top==NULL)
+    return -1;
+    else
+    {
+        int num=top->data;
+        top=top->next;
+        return num;
+        
+    }
+}

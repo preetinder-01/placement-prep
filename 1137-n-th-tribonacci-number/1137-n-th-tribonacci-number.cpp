eb4fe09172ebe5d767b9a3748385d669af1f5b24
Preetinder Singh Bajaj <preetindersinghbajaj@gmail.com>
2022-03-24 13:46:30 +0530

    Time: 0 ms (100.00%), Space: 6 MB (52.08%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        int f[38];
+        f[0]=0;
+        f[1]=f[2]=1;
+        for(int i=0;i<35;i++)
+        {
+            f[i+3]=f[i]+f[i+1]+f[i+2];
+        }
+        return f[n];
+    }
+};

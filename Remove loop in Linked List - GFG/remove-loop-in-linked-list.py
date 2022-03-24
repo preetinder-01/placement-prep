da742f00ad6e553d219d4f29a42e718faf55f197
Preetinder Singh Bajaj <preetindersinghbajaj@gmail.com>
2022-03-24 23:14:41 +0530

    Added solution - LeetHub

---
+++ Remove loop in Linked List - GFG/remove-loop-in-linked-list.py
@@ -0,0 +1,131 @@
+'''
+# node class:
+
+class Node:
+    def __init__(self,val):
+        self.next=None
+        self.data=val
+
+'''
+
+class Solution:
+   #Function to remove a loop in the linked list.
+   def removeLoop(self, head):
+       # code here
+       # remove the loop without losing any nodes
+       
+       if head.next == head or head.next == None:
+           head.next = None
+           return head
+           
+       fast = head.next.next
+       slow = head.next
+       
+       while slow!=fast:
+           if fast == None or fast.next == None or fast.next.next == None:
+               return head
+           else:
+               slow = slow.next
+               fast = fast.next.next
+               
+       order = 1
+       slow = slow.next
+       
+       while slow!= fast:
+           order +=1
+           slow = slow.next
+       
+       slow = head
+       fast = head
+       
+       while order>1:
+           order-=1
+           fast = fast.next
+           
+       if slow == fast.next:
+           fast.next = None
+           return head
+       else:
+           while slow!=fast.next:
+               slow = slow.next
+               fast = fast.next
+               
+           fast.next = None
+           
+           return head
+
+#{ 
+#  Driver Code Starts
+# driver code:
+
+class Node:
+    def __init__(self,val):
+        self.next=None
+        self.data=val
+
+class linkedList:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+    
+    def add(self,num):
+        if self.head is None:
+            self.head=Node(num)
+            self.tail=self.head
+        else:
+            self.tail.next=Node(num)
+            self.tail=self.tail.next
+    
+    def isLoop(self):
+        if self.head is None:
+            return False
+        
+        fast=self.head.next
+        slow=self.head
+        
+        while slow != fast:
+            if fast is None or fast.next is None:
+                return False
+            fast=fast.next.next
+            slow=slow.next
+        
+        return True
+    
+    def loopHere(self,position):
+        if position==0:
+            return
+        
+        walk=self.head
+        for _ in range(1,position):
+            walk=walk.next
+        self.tail.next=walk
+    
+    def length(self):
+        walk=self.head
+        ret=0
+        while walk:
+            ret+=1
+            walk=walk.next
+        return ret
+
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(t):
+        n=int(input())
+        arr=tuple(int(x) for x in input().split())
+        pos=int(input())
+        
+        ll = linkedList()
+        for i in arr:
+            ll.add(i)
+        ll.loopHere(pos)
+        
+        Solution().removeLoop(ll.head)
+        
+        if ll.isLoop() or ll.length()!=n:
+            print(0)
+            continue
+        else:
+            print(1)
+
+# } Driver Code Ends

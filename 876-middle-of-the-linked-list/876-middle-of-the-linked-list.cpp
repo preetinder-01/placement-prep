5894835ad254a7ef264c8394a333898c6834f0fb
Preetinder Singh Bajaj <preetindersinghbajaj@gmail.com>
2022-07-08 18:15:08 +0530

    Time: 3 ms (57.57%), Space: 7.1 MB (71.67%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
@@ -22,16 +22,3 @@ public:
     }
 };
 
-// class Solution {
-// public:
-//     ListNode* middleNode(ListNode* head) {
-//         ListNode* slow=head;
-//         ListNode* fast=head;
-        
-//         while(fast!=NULL && fast->next!=NULL){
-//             slow=slow->next;
-//             fast=fast->next->next;
-//         }
-//         return slow;
-//     }
-// };

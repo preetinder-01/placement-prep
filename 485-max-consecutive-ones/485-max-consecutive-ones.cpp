2b8bcba48f99d8095dc78f5d7c6f30cf3db6ea5d
Preetinder Singh Bajaj <preetindersinghbajaj@gmail.com>
2022-07-12 12:38:42 +0530

    Time: 63 ms (43.33%), Space: 36.2 MB (20.51%) - LeetHub

--- 485-max-consecutive-ones/485-max-consecutive-ones.cpp
+++ 485-max-consecutive-ones/485-max-consecutive-ones.cpp
@@ -5,13 +5,11 @@ public:
         int count=0;
         for(int i=0;i<nums.size();i++)
         {
-            while(i<nums.size() and nums[i]==1)
-            {
-                count++;
-                mx=max(mx,count);
-                i++;
-            }
-            count=0;
+           if(nums[i]==1)
+               count++;
+            else
+                count=0;
+            mx=max(mx,count);
         }
         return mx;
     }

fcf1a122a5073b001fa6b0b23f85311b34b6b3fc
Preetinder Singh Bajaj <preetindersinghbajaj@gmail.com>
2022-07-20 01:30:49 +0530

    Time: 31 ms (98.19%), Space: 89.3 MB (60.88%) - LeetHub

---
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        //as we know n+m+1/2 will give reuired elements to be on each side for both even and odd so first calculate le,l2,r1,r2 then chec for odd even 
+        
+        int m=nums1.size();
+        int n=nums2.size();
+        if(n<m)return findMedianSortedArrays(nums2,nums1);
+        int low=0,high=m;
+        while(low<=high)
+        {
+            int cut1=(low+high)>>1;
+            int cut2=(m+n+1)/2-cut1;
+            int l1=cut1==0?INT_MIN:nums1[cut1-1];
+            int l2=cut2==0?INT_MIN:nums2[cut2-1];
+            int r1=cut1==m?INT_MAX:nums1[cut1];
+            int r2=cut2==n?INT_MAX:nums2[cut2];
+            if(l1<=r2 and l2<=r1)
+            {
+                if( (m+n)%2==0)
+                {
+                    l1=max(l1,l2);
+                    r1=min(r1,r2);
+                    return (double)((l1+r1)/2.0);
+                }
+                else
+                {
+                    return (double)(max(l1,l2));
+                }
+            }
+            else if(l1>r2)
+                high=cut1-1;    // for left shift of cut 1
+            else
+                low=cut1+1;     // for right shift of cut 1
+        
+        }
+        return 0.0;
+    }
+        
+};

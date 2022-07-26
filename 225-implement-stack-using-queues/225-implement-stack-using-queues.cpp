a867e28ec86d631a7bfc2ae589f914aa6fb6698a
Preetinder Singh Bajaj <preetindersinghbajaj@gmail.com>
2022-07-27 00:24:54 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (57.28%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
@@ -1,46 +1,39 @@
 class MyStack {
 public:
- 
     queue<int> q;
-
-    int peek = -1;   //  maintaining the top element of stack 
-    
-    MyStack() {
-       
-    }
+//     MyStack() {
+        
+//     }
     
     void push(int x) {
-         peek = x;
-        q.push(x);
+     q.push(x);
+        for(int i=0;i<q.size()-1;i++)
+        {
+            q.push(q.front());
+            q.pop();
+        }
     }
     
-    
     int pop() {
-        
-       int n = q.size();
-       n-=1; 
-       while(n--){
-          peek = q.front();     // maintining the top element 
-           q.push(peek);
-           q.pop();
-       } 
-        
-       int ret = q.front();     // storing the element to return 
-        q.pop();                // removing the element permanently
-        
-        return ret;
-        
+        int x=q.front();
+        q.pop();
+        return x;
     }
     
     int top() {
-        
-     return peek ;
-        
+        return q.front();
     }
-    
     
     bool empty() {
-        
-        return q.size()==0;
+        return q.empty();
     }
 };
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */

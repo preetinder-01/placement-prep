094e5d777b59cdeea53ab7e4a8f55fe4df04f679
Preetinder Singh Bajaj <preetindersinghbajaj@gmail.com>
2022-08-09 19:51:28 +0530

    Time: 3 ms (96.29%), Space: 7.5 MB (46.42%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -9,6 +9,24 @@ public:
                 str+=s[i];
         }
         // cout<<str;
+        /*
+        
+        for(int i=0;i<s.size();i++){
+            // using isalnum() function, to check,
+            // if s[i] is alphanumeric or not.
+            // if s[i] is alphanumeric, the push that character into str
+            if(isalnum(s[i])){
+                // tolower() is used to convert upper case characters to lower case characters
+                str.push_back(tolower(s[i]));
+            }
+            // if s[i] is not alphanumeric the simply skip it,
+            //because we don't need non-alphanumeric characters
+            else continue;
+        }
+        
+        
+        
+        */
         int i=0,j=str.size()-1;
         while(i<j)
         {
